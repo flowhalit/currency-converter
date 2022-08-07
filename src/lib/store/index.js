3bf113d99b3c5bbbb01f97b4541c5bffe23fe80b
sec-halit <102877470+sec-halit@users.noreply.github.com>
2022-08-07 19:02:39 +0300

    04-clear code 2

--- src/lib/store/index.js
+++ src/lib/store/index.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import currencyReducer from './currency';
-import logger from "redux-logger";
-import thunk from 'redux-thunk'; 
+// import logger from "redux-logger";
+// import thunk from 'redux-thunk'; 
 
-const middleWares = [logger,thunk ];
+// const middleWares = [logger,thunk ];
 export const store = configureStore({
   reducer: {
     currency: currencyReducer,
   },
-  middleware: middleWares
+  // middleware: middleWares
 });
